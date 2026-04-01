18f92444932f6ec20ef397623889525d8d174c05
Anilbk777 <anilbk775@gmail.com>
2026-04-01 20:40:01 +0545

    notification context  added

--- strategy_pattern/notification.py
+++ strategy_pattern/notification.py
@@ -16,3 +16,13 @@ class SMSNotification(NotificationStrategy):
 class PushNotification(NotificationStrategy):
     def send(self, message:str):
         print(f"Sending Push Notification: {message}")
+
+class NotificationContext:
+    def __init__(self, strategy:NotificationStrategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy:NotificationStrategy):
+        self._strategy = strategy
+
+    def notify(self, message:str):
+        self._strategy.send(message)
