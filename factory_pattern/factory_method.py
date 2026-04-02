9a7e4c6aeef12a73b6a0e0d8254300657320584c
Anilbk777 <anilbk775@gmail.com>
2026-04-02 11:49:51 +0545

    factory class added

--- factory_pattern/factory_method.py
+++ factory_pattern/factory_method.py
@@ -8,6 +8,55 @@ class Pizza:
     def __repr__(self):
         return f"{self.name}"
     
+class NYCheesePizza(Pizza):
+
+    def __init__(self):
+        super().__init__("NY style cheese pizza (thin crust)")
+
+class NYPepperoniPizza(Pizza):
+
+    def __init__(self):
+        super().__init__("NY style pepperoni pizza ( thin crust)")
+
+class ChicagoCheesePizza(Pizza):
+
+    def __init__(self):
+        super().__init__("Chicago style cheese pizza (deep dish)")
+
+class ChicagoPepperoniPizza(Pizza):
+
+    def __init__(self):
+        super().__init__("Chicago style pepperoni pizza (deep dish)")
+
+
+class PizzaStore(ABC):
+
+    @abstractmethod
+    def create_pizza(self, pizza_type:str) -> Pizza:
+        pass
+
+    def order_pizza(self, pizza_type:str) -> Pizza:
+        pizza = self.create_pizza(pizza_type)
+        print(f"[Preparing: {pizza}]")
+        return f"ready :{pizza}"
+    
+class NYPizzaStore(PizzaStore):
+
+    def create_pizza(self, pizza_type):
+        if pizza_type == "cheese":
+            return NYCheesePizza()
+
+        elif pizza_type == "pepperoni":
+            return NYPepperoniPizza()
+        
+        else:
+            raise ValueError(f"Unknow NY pizza type: {pizza_type}")
+        
+
+
+    
+
+
 
 
         
