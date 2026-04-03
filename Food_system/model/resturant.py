a4beb982e437b58bb6423fc49be91760fd7b8e11
Anilbk777 <anilbk775@gmail.com>
2026-04-03 13:30:04 +0545

    resturant.py added

---
+++ Food_system/model/resturant.py
@@ -0,0 +1,35 @@
+from .menu_item import MenuItem
+
+class Resturant:
+    RESTURANT_ID = 0
+
+    def __init__(self, name:str, address: str):
+        self._name = name
+        self._address = address
+        self._menu: list[MenuItem] = []
+        Resturant.RESTURANT_ID += 1
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    def set_name(self, n:str):
+        self._name = n
+
+    @property
+    def address(self):
+        return self._address
+    
+    def set_address(self,addr:str):
+        self._address =addr
+
+    @property
+    def menu(self):
+        return self._menu.copy()
+    
+    def add_menu_item(self,menu_item:MenuItem):
+        self._menu.append(menu_item)
+
+    
+
