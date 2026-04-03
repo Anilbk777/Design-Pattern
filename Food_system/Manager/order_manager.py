0e287150f8c65afc48966c20a80dfc900eba6aea
Anilbk777 <anilbk775@gmail.com>
2026-04-03 16:01:03 +0545

    notification_serivces.py and order_manager.py added

---
+++ Food_system/Manager/order_manager.py
@@ -0,0 +1,16 @@
+from model import Order
+from .singleton import singleton
+
+@singleton
+class OrderManager:
+
+    def add_order(self, order: "Order"):
+        self.orders.append(order)
+
+    def list_orders(self):
+        print("\n--- All Orders ---")
+        for order in self.orders:
+            print(
+                f"{order.get_type()} order for {order.get_user().get_name()} | "
+                f"Total: ₹{order.get_total()} | At: {order.get_scheduled()}"
+            )
