cff7debaddb431100ef22ac1df24aaff8f853b26
Anilbk777 <anilbk775@gmail.com>
2026-04-04 17:38:08 +0545

    register_observer method added

--- observer/observer.py
+++ observer/observer.py
@@ -19,3 +19,17 @@ class FitnessDataSubject(ABC):
     @abstractmethod
     def notify(self):
         pass
+
+class FitnessData(FitnessDataSubject):
+    def __init__(self):
+        self.steps = 0
+        self.active_minutes = 0
+        self.calories = 0
+        self.observers :list[FitnessDataObserver] = []
+
+    def register_observer(self, observer:FitnessDataObserver):
+        if not isinstance(observer, FitnessDataObserver):
+            raise ValueError("Give observer is not an instance of FitnessDataObserver")
+        
+        self.observers.append(observer)
+    
