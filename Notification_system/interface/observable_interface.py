fe7a7f4c13d13fdb06b8346baedaa687e88582fe
Anilbk777 <anilbk775@gmail.com>
2026-04-05 18:33:33 +0545

    observer_interface.py added

--- Notification_system/interface/observable_interface.py
+++ Notification_system/interface/observable_interface.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
+from interface.observer_interface import ObserverInterface
 
 class ObservableInterface(ABC):
     @abstractmethod
-    def register_observer(self, observer):
+    def register_observer(self, observer: ObserverInterface):
         pass
 
     @abstractmethod
-    def unregister_observer(self, observer):
+    def unregister_observer(self, observer: ObserverInterface):
         pass
 
     @abstractmethod
