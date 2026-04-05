51602bb4bc246eed5cd8c130aa440233afcd1228
Anilbk777 <anilbk775@gmail.com>
2026-04-05 18:30:27 +0545

    signature decorator added

--- Notification_system/decorator/concrete_decorator.py
+++ Notification_system/decorator/concrete_decorator.py
@@ -7,4 +7,12 @@ class TimeStampDecorator(NotificationDecorator):
         super().__init__(notification)
 
     def get_content(self, message: str):
-        return f"{self._notification.get_content(message)} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        return f"[Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {self._notification.get_content(message)}"
+
+class SignatureDecorator(NotificationDecorator):
+
+    def __init__(self, notification: NotificationInterface):
+        super().__init__(notification)
+
+    def get_content(self, message: str):
+        return f"{self._notification.get_content(message)} [Signature: Admin]"
