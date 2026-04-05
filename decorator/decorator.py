c166ae51b2826ccc00f0794f90b551596db868dc
Anilbk777 <anilbk775@gmail.com>
2026-04-05 09:49:26 +0545

    ItalicDecorator added

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -29,3 +29,12 @@ class BoldDecorator(ITextDecorator):
 
     def render(self) -> str:
         return f"<b> {self.text_view.render()} </b>"
+
+
+class ItalicDecorator(ITextDecorator):
+
+    def __init__(self, text_view: ITextView):
+        super().__init__(text_view)
+
+    def render(self) -> str:
+        return f"<i> {self.text_view.render()} </i>"
