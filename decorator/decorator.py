3fc5b571327a7a378a818ed2018cad0ae7383d4f
Anilbk777 <anilbk775@gmail.com>
2026-04-05 09:40:17 +0545

    PlainTextView and ITextDecoartor added

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -6,3 +6,18 @@ class ITextView(ABC):
     def render(self) -> str:
         pass
 
+class PlainTextVIew(ITextView):
+
+    def __init__(self, text:str):
+        self.text = text
+
+    def render(self) -> str:
+        return self.text
+    
+class ITextDecorator(ITextView):
+    
+    def __init__(self, text_view:ITextView):
+        self.text_view = text_view
+
+    def render(self):
+        pass
