586e96d955210d7b090ef2b12cbee2bb91cde0a9
Anilbk777 <anilbk775@gmail.com>
2026-04-05 09:45:22 +0545

    BoldDecorator added

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -6,14 +6,14 @@ class ITextView(ABC):
     def render(self) -> str:
         pass
 
-class PlainTextVIew(ITextView):
+class PlainTextView(ITextView):
 
     def __init__(self, text:str):
         self.text = text
 
     def render(self) -> str:
         return self.text
-    
+
 class ITextDecorator(ITextView):
     
     def __init__(self, text_view:ITextView):
@@ -21,3 +21,11 @@ class ITextDecorator(ITextView):
 
     def render(self):
         pass
+
+class BoldDecorator(ITextDecorator):
+
+    def __init__(self, text_view: ITextView):
+        super().__init__(text_view)
+
+    def render(self) -> str:
+        return f"<b> {self.text_view.render()} </b>"
