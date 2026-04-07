f606108358a4452dfad6d30172aa166f263b1e8f
Anilbk777 <anilbk775@gmail.com>
2026-04-07 16:53:41 +0545

    ModelTrainer class added

---
+++ template/template_method.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+class ModelTrainer(ABC):
+    def train_pipeline(self, data_path: str) -> None:
+        try:
+            self._load_data(data_path)
+            self._preprocess_data()
+            self.train_model()
+            self.evaluate_model()
+            self.save_model()
+        except Exception as e:
+            logging.error(f"[Error] Pipeline failed: {e}")
+            raise
+
+    # Common steps
+    def _load_data(self, path: str) -> None:
+        logging.info(f"[Common] Loading dataset from {path}")
+
+    def _preprocess_data(self) -> None:
+        logging.info("[Common] Splitting into train/test and normalizing")
+
+    @abstractmethod
+    def train_model(self) -> None:
+        pass
+
+    @abstractmethod
+    def evaluate_model(self) -> None:
+        pass
+
+    def save_model(self) -> None:
+        logging.info("[Common] Saving model to disk as default format")
+
