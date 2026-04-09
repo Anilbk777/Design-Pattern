264137348c0eeba271568c09f5fcb8a046c82118
Anilbk777 <anilbk775@gmail.com>
2026-04-09 21:43:28 +0545

    class request  added

--- chain of responsibility/chain_of_responsibility.py
+++ chain of responsibility/chain_of_responsibility.py
@@ -1,10 +1,31 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class Request:
+    def __init__(self, user, token, role, request_count, payload):
+        self.user = user
+        self.token = token
+        self.role = role
+        self.request_count = request_count
+        self.payload = payload
+
 
 class Handler(ABC):
-    @abstractmethod
-    def set_next(self, handler:"Handler"):
-        pass
+    def __init__(self):
+        self.next_handler: Optional["Handler"] | None = None
+
+    def set_next(self, handler: "Handler") -> "Handler":
+        self._next = handler
+        return handler
+
+    def handle(self, request: Request):
+        if not self._process(request):
+            return  
+
+        if self._next:
+            return self._next.handle(request)
 
     @abstractmethod
-    def handle_request(self, request):
+    def _process(self, request: Request) -> bool:
         pass
