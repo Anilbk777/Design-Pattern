89276cd34a4bbbcb06b27d4e840c2f93ef3c98e0
Anilbk777 <anilbk775@gmail.com>
2026-04-10 19:00:45 +0545

    fix the payment controller issue(to make it singleton)

--- payment_gateway/payment_gateway.py
+++ payment_gateway/payment_gateway.py
@@ -8,36 +8,39 @@ import random
 
 @dataclass
 class PaymentRequest:
-    sender : str
-    receiver : str
-    amount : float
-    currency : str
+    sender: str
+    receiver: str
+    amount: float
+    currency: str
+
 
 class BankingSystem(ABC):
 
     @abstractmethod
-    def process_payment(self, amount:float) -> bool:
+    def process_payment(self, amount: float) -> bool:
         pass
 
 
 class PaytmBankingSystem(BankingSystem):
 
-    def process_payment(self, amount:float) -> bool:
+    def process_payment(self, amount: float) -> bool:
         r = (random.randrange(10, 100)) % 100
-        return r > 30
+        return r > 80
+
 
 class RazorpayBankingSystem(BankingSystem):
 
-    def process_payment(self, amount:float) -> bool:
+    def process_payment(self, amount: float) -> bool:
         r = (random.randrange(10, 100)) % 100
         return r > 20
+
 
 class PaymentGateway(ABC):
 
     def __init__(self):
-        self.banking_system : Optional[BankingSystem] | None = None
+        self.banking_system: Optional[BankingSystem] = None
 
-    def process_payment(self, payment_request:PaymentRequest) -> bool:
+    def process_payment(self, payment_request: PaymentRequest) -> bool:
         if not (self.validate_payment(payment_request)):
             print(f"[PaymentGateway] validation failed for {payment_request.sender}.")
             return False
@@ -51,15 +54,15 @@ class PaymentGateway(ABC):
         return True
 
     @abstractmethod
-    def validate_payment(self, payment_request:PaymentRequest) -> bool:
+    def validate_payment(self, payment_request: PaymentRequest) -> bool:
         pass
 
     @abstractmethod
-    def initiate_payment(self, payment_request:PaymentRequest) -> bool:
+    def initiate_payment(self, payment_request: PaymentRequest) -> bool:
         pass
 
     @abstractmethod
-    def confirm_payment(self,payment_request:PaymentRequest) -> bool:
+    def confirm_payment(self, payment_request: PaymentRequest) -> bool:
         pass
 
 
@@ -67,19 +70,21 @@ class PaytmGateway(PaymentGateway):
     def __init__(self):
         self.banking_system = PaytmBankingSystem()
 
-    def validate_payment(self, payment_request:PaymentRequest) -> bool:
+    def validate_payment(self, payment_request: PaymentRequest) -> bool:
         print(f"[Paytm] validating payment for {payment_request.sender}")
 
-        if payment_request.amount <= 0 or payment_request.currency != "NRP":
+        if payment_request.amount <= 0 or payment_request.currency != "NPR":
             return False
         return True
 
-    def initiate_payment(self, payment_request:PaymentRequest) -> bool:
-        print(f"[Paytm] initiating payment of {payment_request.amount} {payment_request.currency} for {payment_request.sender}")
+    def initiate_payment(self, payment_request: PaymentRequest) -> bool:
+        print(
+            f"[Paytm] initiating payment of {payment_request.amount} {payment_request.currency} for {payment_request.sender}"
+        )
 
         return self.banking_system.process_payment(payment_request.amount)
 
-    def confirm_payment(self,payment_request:PaymentRequest) -> bool:
+    def confirm_payment(self, payment_request: PaymentRequest) -> bool:
         print(f"[Paytm] Confirming payment for {payment_request.sender}")
         return True
 
@@ -106,28 +111,34 @@ class RazorpayGateway(PaymentGateway):
         print(f"[Razorpay] Confirming payment for {payment_request.sender}")
         return True
 
+
 class PaymentGatewayProxy(PaymentGateway):
-    def __init__(self,payment_gateway:PaymentGateway, max_retries:int):
+    def __init__(self, payment_gateway: PaymentGateway, max_retries: int):
         self.payment_gateway = payment_gateway
         self.max_retries = max_retries
 
-    def process_payment(self, payment_request:PaymentRequest) -> bool:
+    def process_payment(self, payment_request: PaymentRequest) -> bool:
         result = False
         for attempt in range(self.max_retries + 1):
-            if (attempt > 0):
-                print(f"[Proxy] Retrying payment (attemp - {attempt}) for {payment_request.sender}.")
-            
-            result = self.payment_gateway.process_payment(payment_request)
-            if result: break 
+            if attempt > 0:
+                print(
+                    f"[Proxy] Retrying payment (attempt - {attempt}) for {payment_request.sender}."
+                )
 
-        if (not result):
-            print(f"[Proxy] Payment failed after {self.max_retries} attempts for {payment_request.sender}")
-        
+            result = self.payment_gateway.process_payment(payment_request)
+            if result:
+                break
+
+        if not result:
+            print(
+                f"[Proxy] Payment failed after {self.max_retries} attempts for {payment_request.sender}"
+            )
+
         return result
-    
+
     def validate_payment(self, payment_request):
         return self.payment_gateway.validate_payment(payment_request)
-    
+
     def initiate_payment(self, payment_request):
         return self.payment_gateway.initiate_payment(payment_request)
 
@@ -141,23 +152,24 @@ class GatewayType(Enum):
 
 
 class GatewayFactory:
-    def get_getway(self, type: GatewayType) -> PaymentGateway:
+    def get_gateway(self, type: GatewayType) -> PaymentGateway:
         if type == GatewayType.PAYTM:
             paytm_gateway = PaytmGateway()
-            return PaymentGatewayProxy(paytm_gateway,4)
-        
+            return PaymentGatewayProxy(paytm_gateway, 4)
+
         else:
             razorpay_gateway = RazorpayGateway()
             return PaymentGatewayProxy(razorpay_gateway, 2)
 
+
 class PaymentService:
     def __init__(self):
-        self.payment_gateway :Optional[PaymentGateway] | None = None
+        self.payment_gateway: Optional[PaymentGateway] = None
 
     def set_gateway(self, payment_gateway: PaymentGateway):
         self.payment_gateway = payment_gateway
 
-    def process_payment(self, payment_request:PaymentRequest):
+    def process_payment(self, payment_request: PaymentRequest):
         if not self.payment_gateway:
             print(f"[PaymentService] No payment gateway selected.")
             return False
@@ -167,26 +179,30 @@ class PaymentService:
 
 class PaymentController:
 
-    def handle_payment(self, gateway_type:GatewayType, payment_request: PaymentRequest):
-        payment_gateway = GatewayFactory().get_getway(gateway_type)
-        PaymentService().set_gateway(payment_gateway)
-        return PaymentService().process_payment(payment_request)
+    def handle_payment(
+        self, gateway_type: GatewayType, payment_request: PaymentRequest
+    ):
+        payment_gateway = GatewayFactory().get_gateway(gateway_type)
+        service = PaymentService()
+        service.set_gateway(payment_gateway)
+        return service.process_payment(payment_request)
 
 
 if __name__ == "__main__":
-    payment_request1 = PaymentRequest("Ram","shyam",500,"NRP")
+    payment_request1 = PaymentRequest("Ram", "shyam", 500, "NPR")
 
     print("Processing via Paytm")
-    print("="*60)
+    print("=" * 60)
     payment_controller = PaymentController()
-    result1 = payment_controller.handle_payment("paytm",payment_request1)
+    result1 = payment_controller.handle_payment(GatewayType.PAYTM, payment_request1)
     print("SUCCESS" if result1 else "FAIL")
+    print("=" * 60)
     print("=" * 60)
 
     payment_request2 = PaymentRequest("David", "John", 500, "USD")
     print("Processing via Razorpay")
     print("=" * 60)
     payment_controller = PaymentController()
-    result2 = payment_controller.handle_payment("razorpay", payment_request2)
+    result2 = payment_controller.handle_payment(GatewayType.RAZORPAY, payment_request2)
     print("SUCCESS" if result2 else "FAIL")
     print("=" * 60)
