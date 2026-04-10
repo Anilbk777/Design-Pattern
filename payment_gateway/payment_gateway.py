04ee6fe42aa708d06b06988292e404b84a56153b
Anilbk777 <anilbk775@gmail.com>
2026-04-10 15:51:14 +0545

    BankingSystem classes added

--- payment_gateway/payment_gateway.py
+++ payment_gateway/payment_gateway.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+import random
+
+
 @dataclass
 class PaymentRequest:
     sender : str
@@ -8,3 +11,21 @@ class PaymentRequest:
     amount : float
     currency : str
 
+class BankingSystem(ABC):
+
+    @abstractmethod
+    def process_payment(amount:float) -> bool:
+        pass
+
+
+class PaytmBankingSystem(BankingSystem):
+
+    def process_payment(amount:float) -> bool:
+        r = (random.randrange(10, 100)) % 100
+        return r > 30
+
+class RazorpayBankingSystem(BankingSystem):
+
+    def process_payment(amount:float) -> bool:
+        r = (random.randrange(10, 100)) % 100
+        return r > 20
